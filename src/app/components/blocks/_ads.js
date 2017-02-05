4f9788df409c7c2172809da44a9b7c8151a84a39
s.arendarchuk <ipmazahell@gmail.com>
2017-02-06 00:40:57 +0200

    Add change system, change type prices, start calculating list

--- src/app/components/blocks/_ads.js
+++ src/app/components/blocks/_ads.js
@@ -2,19 +2,19 @@ import React, {Component} from 'react'
 
 class Ads extends Component {
 
-  componentDidMount() {
-    if (!document.getElementById(window.AID)) {
-      let e = document.createElement("div")
-      e.id = window.AID
-      e.style = "font-size: 1.4em;margin-bottom: 3px;padding: 10px;background: rgba(39,39,40,0.9);text-align: center;font-weight: bold;color: #fff"
-      e.innerHTML ="Advertising helps me to maintain my website and integrate new functionality." +
-        "<br />"+
-        "So if you want to support the project, you just need to turn off your <span style='color: #DF4A32'>AdBlock extension</span>";
-      let div = window.document.getElementById("aZone")
-      div.style.display = 'block'
-      div.innerHTML = e.outerHTML
-    }
-  }
+  // componentDidMount() {
+  //   if (!document.getElementById(window.AID)) {
+  //     let e = document.createElement("div")
+  //     e.id = window.AID
+  //     e.style = "font-size: 1.4em;margin-bottom: 3px;padding: 10px;background: rgba(39,39,40,0.9);text-align: center;font-weight: bold;color: #fff"
+  //     e.innerHTML ="Advertising helps me to maintain my website and integrate new functionality." +
+  //       "<br />"+
+  //       "So if you want to support the project, you just need to turn off your <span style='color: #DF4A32'>AdBlock extension</span>";
+  //     let div = window.document.getElementById("aZone")
+  //     div.style.display = 'block'
+  //     div.innerHTML = e.outerHTML
+  //   }
+  // }
   render() {
     return <div id="aZone"></div>
   }
