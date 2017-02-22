078424c4e4ecc28f711d36d7b1d84efc5c75105a
s.arendarchuk <ipmazahell@gmail.com>
2017-02-22 17:12:03 +0200

    Add opacity to planet materials

--- src/app/components/resourses/planet/OneTable.js
+++ src/app/components/resourses/planet/OneTable.js
@@ -6,7 +6,7 @@ const OneTable = ({_class, data, hr, lh, title}) => {
   let _handleClick = (itemId, url, e) => lh(itemId, url, e)
 
   let materials = data.map(val => {
-    return <li className="pea" onClick={_handleClick.bind(this, val.item_id, val.url)} data-parent={val.item_id} data-childs={'-' + val.rel + '-'} key={val.item_id} onMouseOver={_handleOnMouse.bind(this, val.item_id)}>
+    return <li className="pea opacity05" onClick={_handleClick.bind(this, val.item_id, val.url)} data-parent={val.item_id} data-childs={'-' + val.rel + '-'} key={val.item_id} onMouseOver={_handleOnMouse.bind(this, val.item_id)}>
       <div style={{'pointerEvents': 'none'}} className='m-b-1'>
         <img
           className='img24 pen'
